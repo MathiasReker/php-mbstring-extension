ed4c0d999ae076ebd9d5ef3a0b9b1e0678e22c9a
Kaga Akatsuki <filberttkarry2210@gmail.com>
2025-05-08 10:24:37 +0700

    rewrite logic ucwords

--- tests/Unit/MbUcwordsTest.php
+++ tests/Unit/MbUcwordsTest.php
@@ -26,6 +26,7 @@ final class MbUcwordsTest extends TestCase
     {
         $this->assertEquals('The Quick Brown Fox', Mbstring::ucwords('the quick brown fox'));
         $this->assertEquals('The QUiCK BrOwn FoX', Mbstring::ucwords('the qUiCK brOwn foX'));
+        $this->assertEquals('The-Quick-Brown-Fox', Mbstring::ucwords('the-quick-brown-fox', '-'));
     }
 
     public function testHandlesNonASCIICharacters(): void
